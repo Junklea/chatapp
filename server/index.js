376664260606f46f906b621780ee74c812d2cb0f
Ismail Tarik Erkan <taricerkan@gmail.com>
2022-01-27 19:15:08 +0300

    database dotenv login signup setup

--- server/index.js
+++ server/index.js
@@ -4,8 +4,10 @@ const app = express();
 const helmet = require('helmet');
 const cors = require('cors');
 const authRouter = require('./routers/authRouter');
+const session = require('express-session');
 
 const server = require('http').createServer(app);
+require('dotenv').config();
 
 const io = new Server(server, {
   cors: {
@@ -22,7 +24,21 @@ app.use(
   })
 );
 app.use(express.json());
-
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    credentials: true,
+    name: 'sid',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.ENVIRONMENT === 'production' ? 'true' : 'auto',
+      httpOnly: true,
+      expires: 1000 * 60 * 60 * 24 * 7,
+      sameSite: process.env.ENVIRONMENT === 'production' ? 'none' : 'lax',
+    },
+  })
+);
 app.use('/auth', authRouter);
 
 app.get('/', (req, res) => {
